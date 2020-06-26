b106ee5e476576e055aa0007c6e2c040706773ec
Yamen Barakat <yamen.barakat.1994@gmail.com>
2020-06-27 01:26:28 +0300

    import css files

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,12 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+import './styles/resets.css'
+import './styles/base.css'
+import './styles/header.css'
+import './styles/form.css'
+import './styles/footer.css'
+
 console.log(checkForName);
 
 console.log("CHANGE!!");
