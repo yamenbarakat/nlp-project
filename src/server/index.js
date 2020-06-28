49cd587e7402c5cd55f1aa3bf0a9f7b3f58386c4
Yamen Barakat <yamen.barakat.1994@gmail.com>
2020-06-28 22:28:25 +0300

    set up aylien

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,17 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
 const app = express()
+const aylinet = require('aylien_textapi');
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+const textapi = new aylien({
+  application_id: "a5add80e",
+  application_key: "fec8ea8aa52aa5f7c1e10bc2f649b8cc"
+});
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
