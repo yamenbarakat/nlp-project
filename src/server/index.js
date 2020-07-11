92e03f751a3d91fbf27f2b27cc4e695526149e3d
Yamen Barakat <yamen.barakat.1994@gmail.com>
2020-07-12 02:32:02 +0300

    a

--- src/server/index.js
+++ src/server/index.js
@@ -53,8 +53,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8000, function () {
+    console.log('Example app listening on port 8000!')
 })
 
 app.get('/test', function (req, res) {
